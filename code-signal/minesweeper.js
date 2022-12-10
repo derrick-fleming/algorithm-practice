79dc4d61b242001dc78b10d52b7e328693bcf31d
Derrick Fleming <derrick.fleming7@gmail.com>
2022-12-10 22:57:52 +0000

    Solution to Minesweeper algorithm challenge in CodeSignal's Arcade Intro.

---
+++ code-signal/minesweeper.js
@@ -0,0 +1,61 @@
+function minesweeper(matrix) {
+  const matrixXCoordinateMax = matrix[0].length - 1;
+  const matrixYCoordinateMax = matrix.length - 1;
+
+  const minesArray = [];
+  for (let i = 0; i <= matrixYCoordinateMax; i++) {
+    minesArray.push([]);
+  }
+  // loop through the matrix to find if it's neighboring elements are true.
+  for (let yCoordinate = 0; yCoordinate <= matrixYCoordinateMax; yCoordinate++) {
+    for (let xCoordinate = 0; xCoordinate <= matrixXCoordinateMax; xCoordinate++) {
+      let mines = 0;
+      if (yCoordinate !== 0) {
+        if (xCoordinate !== 0) {
+          if (matrix[yCoordinate - 1][xCoordinate - 1]) {
+            mines++;
+          }
+        }
+        if (matrix[yCoordinate - 1][xCoordinate]) {
+          mines++;
+        }
+        if (xCoordinate !== matrixXCoordinateMax) {
+          if (matrix[yCoordinate - 1][xCoordinate + 1]) {
+            mines++;
+          }
+        }
+      }
+      if (xCoordinate !== 0) {
+        if (matrix[yCoordinate][xCoordinate - 1]) {
+          mines++;
+        }
+      }
+      if (yCoordinate !== matrixYCoordinateMax) {
+        if (matrix[yCoordinate + 1][xCoordinate - 1]) {
+          mines++;
+        }
+        if (xCoordinate !== matrixXCoordinateMax) {
+          if (matrix[yCoordinate + 1][xCoordinate + 1]) {
+            mines++;
+          }
+        }
+        if (matrix[yCoordinate + 1][xCoordinate]) {
+          mines++;
+        }
+      }
+      if (xCoordinate !== matrixXCoordinateMax) {
+        if (matrix[yCoordinate][xCoordinate + 1]) {
+          mines++;
+        }
+      }
+      minesArray[yCoordinate].push(mines);
+    }
+
+  }
+  return minesArray;
+
+}
+
+minesweeper([[1, 2, 1],
+  [2, 1, 1],
+  [1, 1, 1]]);
