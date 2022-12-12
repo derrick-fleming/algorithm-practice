cfbaaf24d658848a1c2565a210a833d41e1275c5
Derrick Fleming <derrick.fleming7@gmail.com>
2022-12-12 01:03:13 +0000

    Updated refactor-pick.js file to include the solution with array method included in solution.

---
+++ code-signal/refactor-pick.js
@@ -0,0 +1,11 @@
+function pick(source, keys) {
+  const pickObject = {};
+  for (const key in source) {
+    if (keys.includes(key) && source[key] !== undefined) {
+      pickObject[key] = source[key];
+    }
+  }
+  return pickObject;
+}
+
+pick({ foo: 1, bar: 2, baz: 3 }, ['foo', 'baz']);
