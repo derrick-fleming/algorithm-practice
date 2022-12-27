05e19000ddfc5b4ff7bae6738669655ccd369cfc
Derrick Fleming <derrick.fleming7@gmail.com>
2022-12-27 00:56:29 +0000

    Completed longestDigitPrefix challenge in Code Signal's Arcade Intro.

---
+++ code-signal/longest-digit-prefix.ts
@@ -0,0 +1,13 @@
+function longestDigitPrefix(inputString: string): string {
+  let outputString: string = ''
+  for (let i = 0; i < inputString.length; i++) {
+    if (Number.isInteger(Number(inputString[i])) && inputString[i] !== ' ') {
+      outputString = outputString + inputString[i]
+    } else {
+      return outputString;
+    }
+  }
+  return outputString;
+}
+
+longestDigitPrefix("the output is 42");
