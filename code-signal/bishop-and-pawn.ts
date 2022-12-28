8b60da8dd77befa7b0901607465affa8c5901ae7
Derrick Fleming <derrick.fleming7@gmail.com>
2022-12-28 22:32:12 +0000

    Completed Bishop and Pawn challenge in Code Signal's Arcade Intro.

---
+++ code-signal/bishop-and-pawn.ts
@@ -0,0 +1,9 @@
+function bishopAndPawn(bishop: string, pawn: string): boolean {
+  const bishopChar = bishop.charCodeAt(0);
+  const pawnChar = pawn.charCodeAt(0);
+  const slope = Math.abs((bishopChar - pawnChar) / (Number(bishop[1]) - Number(pawn[1])))
+  if (slope !== 1) return false;
+  return true;
+}
+
+bishopAndPawn('d5', 'h1');
