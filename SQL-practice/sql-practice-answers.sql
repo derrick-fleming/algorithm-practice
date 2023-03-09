1677bb94eb532a760d74e3bda08a3f76426d0a74
Derrick Fleming <derrick.fleming7@gmail.com>
2023-03-09 23:30:57 +0000

    Completed 5 additional sql tasks querying for birth-dates, specific heights, specific patient ids, city names by joining tables, and concatinating names.

--- SQL-practice/sql-practice-answers.sql
+++ SQL-practice/sql-practice-answers.sql
@@ -26,3 +26,33 @@ where weight between 100 and 120
 update patients
 set "allergies" = 'NKA'
 where allergies is null
+
+/* Question 6 - Show first name and last name concatinated into one column to show their full name. */
+select concat(first_name, " ", last_name) as fullName
+from patients
+
+/* Question 7 - Show first name, last name, and the full province name of each patient.
+Example: 'Ontario' instead of 'ON' */
+select first_name,
+	   last_name,
+       province_name
+from patients
+join province_names using (province_id)
+
+/* Question 8 - Show how many patients have a birth_date with 2010 as the birth year. */
+select count(birth_date)
+from patients
+where birth_date like '2010%'
+
+/* Question 9 - Show the first_name, last_name, and height of the patient with the greatest height. */
+select first_name,
+	   last_name,
+       height
+from patients
+order by height desc
+limit 1
+
+/* Question 10 - Show all columns for patients who have one of the following patient_ids: 1,45,534,879,1000 */
+select *
+from patients
+where patient_id In (1,45,534,879,1000)
