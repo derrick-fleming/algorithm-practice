a60a3216b33830ffea0a403ebfa92f575ac32e68
Derrick Fleming <derrick.fleming7@gmail.com>
2023-03-10 19:12:29 +0000

    Completed rest of easy exercises in sql-practice.

--- SQL-practice/sql-practice-answers.sql
+++ SQL-practice/sql-practice-answers.sql
@@ -56,3 +56,48 @@ limit 1
 select *
 from patients
 where patient_id In (1,45,534,879,1000)
+
+/* Question 11 - Count all admissions */
+select Count()
+from admissions
+
+/* Question 12 - Show all the columns from admissions where the patient was admitted and discharged on the same day. */
+select *
+from admissions
+where admission_date = discharge_date;
+
+/* Question 13 - Show the patient id and the total number of admissions for patient_id 579. */
+select patient_id,
+       count (admission_date)
+from admissions
+where patient_id = 579
+
+/* Question 14 - Based on the cities that our patients live in, show unique cities that are in province_id 'NS'? */
+select DISTINCT city
+from patients
+where province_id = 'NS'
+
+/* Question 15 - Write a query to find the first_name, last name and birth date of patients who has height greater than 160 and weight greater than 70 */
+select first_name,
+       last_name,
+       birth_date
+from patients
+where height > 160 and weight > 70
+
+/* Question 16 - Write a query to find list of patients first_name, last_name, and allergies from Hamilton where allergies are not null */
+select first_name,
+       last_name,
+       allergies
+from patients
+where city = 'Hamilton' and allergies not null
+
+/* Question 17 - Based on cities where our patient lives in, write a query to display the list of unique city starting with a vowel (a, e, i, o, u). Show the result order in ascending by city. */
+select distinct city
+from patients
+where
+   city like 'a%'
+   or city like 'e%'
+   or city LIke 'i%'
+   or city Like 'o%'
+   or city Like 'u%'
+order by city
